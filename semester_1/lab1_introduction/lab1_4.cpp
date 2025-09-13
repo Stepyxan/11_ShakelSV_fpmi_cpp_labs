3cccfc42875781b5b7879fb1eaef29cbcf8e2903
Steve <stepyxan@gmail.com>
2025-09-13 23:45:10 +0300

    lab1 tasks changed

--- semester_1/lab1_introduction/lab1_4.cpp
+++ semester_1/lab1_introduction/lab1_4.cpp
@@ -4,7 +4,10 @@ int main()
 {
     int k;
     int d_factorial = 1;
-    std::cin >> k;
+    if (!(std::cin >> k) || k < 1) {
+        std::cout << "error";
+        std::exit(1);
+    }
     for (int i = k; i >= 1; i -= 2) {
         d_factorial *= i;
     }
--- semester_1/lab1_introduction/lab1_7.cpp
+++ semester_1/lab1_introduction/lab1_7.cpp
@@ -3,12 +3,17 @@
 int main()
 {
     int n;
-    std::cin >> n;
     double d;
-    std::cin >> d;
+    if (!(std::cin >> n >> d) || !(n > 0)) {
+        std::cout << "error";
+        std::exit(1);
+    }
     double mx = d, mn = d, av = d;
     for (int i = 1; i < n; i++) {
-        std::cin >> d;
+        if (!(std::cin >> d)) {
+            std::cout << "error";
+            std::exit(1);
+        }
         if (d > mx) {
             mx = d;
         }
