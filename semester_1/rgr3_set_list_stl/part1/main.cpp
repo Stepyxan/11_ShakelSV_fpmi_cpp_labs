f0aa200d69c60fb495a2a61287017977d9af6ae4
Steve <stepyxan@gmail.com>
2025-12-22 17:03:01 +0300

    rgr3 part1 added

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <set>
 
-
+std::set<int> compute_primes(int n) {
+    std::set<int> primes;
+    std::vector<bool> is_prime(n + 1);
+    for (int i = 2; i <= n; ++i) {
+        is_prime[i] = true;
+    }
+    for (int i = 2; i <= n; ++i) {
+        if (is_prime[i]) {
+            primes.insert(i);
+        }
+        for (int j = i * i; j <= n; j += i) {
+            is_prime[j] = false;
+        }
+    }
+    return primes;
+}
 
 int main() {
-
-    // your code goes here
-
-    return 0;
+    int n;
+    std::cout << "Input a natural number N: \n";
+    if (!(std::cin >> n) || (n < 1)) {
+        std::cerr << "Error: N must be natural";
+        std::exit(1);
+    }
+    std::set<int> primes = compute_primes(n);
+    std::cout << "Primes no greater than N: \n";
+    for(std::set<int>::iterator it = primes.begin(); it != primes.end(); ++it) {
+        std::cout << *it << ' ';
+    }
 }
